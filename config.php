f65a80edf5a37aa56b011f6b9f5bf5ca30f0e5f8
c0m4r <github@wolfet.pl>
2023-12-20 12:42:01 +0100

    Create config.php

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+
+$config = array(
+        "input_filename"        => 'ICAfYf6c7fTuyIxV2PBX', // put something random here
+        "upload_dir"            => 'i', // final image destination
+        "logs_dir"              => 'logs', // where logs are stored
+        "max_filesize"          => '10485760', // max file size (in bytes)
+        "files_limit"           => '100' // global limit of how many files can be stored
+);
+
+?>
