84088018f9cddbd7fe2854f9a96d9c1643e03681
c0m4r <github@wolfet.pl>
2023-12-21 08:56:50 +0100

    Update lang.php
    changed formatting

--- lang.php
+++ lang.php
@@ -1,25 +1,17 @@
 <?php
 
-// Lang
-
-if(isset($_SERVER['HTTP_ACCEPT_LANGUAGE']))
-{
-	$lang = substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2);
-}
-else
-{
-	$lang = 'en';
+if(isset($_SERVER['HTTP_ACCEPT_LANGUAGE'])) {
+    $lang = substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2);
+} else {
+    $lang = 'en';
 }
 
 switch($lang)
 {
-	case 'pl':
-	
-		require("lang/pl.php"); break;
-		
-	default:
-		
-		require("lang/en.php"); break;
+    case 'pl':
+	require("lang/pl.php"); break;
+    default:
+        require("lang/en.php"); break;
 }
 
 ?>
