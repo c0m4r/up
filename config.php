531b8e74f82a274ab4328e4222fb56aeb5de235a
c0m4r <github@wolfet.pl>
2023-12-21 19:29:09 +0100

    Update config.php
    Changed input_filename as the previous method won't work if behind reverse proxy

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 
 $config = array(
-        "input_filename"        => hash('whirlpool', $_SERVER["REMOTE_ADDR"]), // put something unique here, though I have no idea what this was for :D
+        "input_filename"        => 'RBZwvih2S1cLg4X25E0ZyylPbdGh97CA', // put something unique here, though I have no idea what this was for :D
         "upload_dir"            => 'i', // final image destination
         "logs_dir"              => 'logs', // where logs are stored
         "max_filesize"          => '10485760', // max file size (in bytes)
