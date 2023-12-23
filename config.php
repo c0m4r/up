c2b953436722248299f984d251cf7bde3e8c2b31
c0m4r <github@wolfet.pl>
2023-12-23 01:12:57 +0100

    increased files_limit config option

--- config.php
+++ config.php
@@ -5,7 +5,7 @@ $config = array(
         "upload_dir"            => 'i', // final image destination
         "logs_dir"              => 'logs', // where logs are stored
         "max_filesize"          => '10485760', // max file size (in bytes)
-        "files_limit"           => '100' // global limit of how many files can be stored
+        "files_limit"           => '1000' // global limit of how many files can be stored
 );
 
 ?>
