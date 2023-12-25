07f7c551e138345839a04001e408da610384d363
c0m4r <github@wolfet.pl>
2023-12-25 15:20:27 +0100

    new language: fr (français)

--- lang.php
+++ lang.php
@@ -12,6 +12,8 @@ switch($lang)
 	require("lang/pl.php"); break;
     case 'uk':
         require("lang/uk.php"); break;
+    case 'fr':
+        require("lang/fr.php"); break;
     default:
         require("lang/en.php"); break;
 }
---
+++ lang/fr.php
@@ -0,0 +1,26 @@
+<?php
+
+$lang = array
+(
+	"zajete_miejsce" 			=> "Utilisation de l'espace disque",
+	"wgrywaj_screeny" 			=> "Téléchargez des captures d'écran et des photos à la vitesse des photons",
+	"wybierz_plik" 				=> "Sélectionnez le fichier et attendez, c'est automatique !",
+	"wlacz_javascript"			=> "Activez JavaScript s'il vous plaît. Je ne peux pas profiter de la vie sans ça :(",
+	"plik_zbyt_duzy" 			=> "Fichier trop large.",
+	"blad_podczas_przesylania" 		=> "Erreur. Assurez-vous que le fichier est une image.",
+	"wgrywanie_przerwane" 			=> "Téléchargement annulé.",
+	"blad_odczytu_postepu" 			=> "Impossible de progresser",
+	"koniec_miejsca" 			=> "Limite d'espace dépassée.",
+	"nieobslugiwany_typ" 			=> "Type de fichier non pris en charge.",
+	"nieprawidlowy_format" 			=> "Format de fichier invalide.",
+	"obiekt_nie_jest_graficzny" 		=> "L'objet n'est pas un fichier graphique.",
+	"najpierw_wybierz_plik" 		=> "Tout d’abord, sélectionnez un fichier.",
+	"regulamin"				=> "Conditions d'utilisation",
+	"regulamin_tekst"                       => "Ne sois pas méchant.",
+	"polityka_prywatnosci"			=> "politique de confidentialité",
+	"polityka_prywatnosci_tekst"            => "Nous enregistrerons votre adresse IP et l'heure de téléchargement pour des raisons de sécurité.",
+	"kontakt"				=> "Contact",
+	"kontakt_tekst"                         => "Contactez-nous ici et là."
+);
+
+?>
