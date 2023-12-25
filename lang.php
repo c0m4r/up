3c7189ae200f0631d9beb2cd42121495d8c2daac
c0m4r <github@wolfet.pl>
2023-12-25 15:10:47 +0100

    new language: uk (українська) typo

--- lang.php
+++ lang.php
@@ -1,7 +1,5 @@
 <?php
 
-print_r($_SERVER['HTTP_ACCEPT_LANGUAGE']);
-
 if(isset($_SERVER['HTTP_ACCEPT_LANGUAGE'])) {
     $lang = substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2); echo $lang;
 } else {
@@ -13,7 +11,7 @@ switch($lang)
     case 'pl':
 	require("lang/pl.php"); break;
     case 'uk':
-        require("lang/ua.php"); break;
+        require("lang/uk.php"); break;
     default:
         require("lang/en.php"); break;
 }
